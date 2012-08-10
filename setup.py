3e76d35c84e7d2e8975b4ddc92d9fec8592cd3fa
Dominique B <domi_burnand@yahoo.de>
2012-08-10 14:05:59 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+#===============================================================================
+# Written by Rentouch 2012 - http://www.rentouch.ch
+#===============================================================================
+
+import os
+import shutil
+
+def install():
+    #search pbase folder
+    home=os.path.expanduser("~")
+    git=os.getcwd()
+    if not os.path.isdir(os.path.join(home, "PBase")):
+        print "can not locate ~/PBase folder"
+        return
+    
+    #create PBase/PBController folder if it doesn't exist
+    if not os.path.isdir(os.path.join(home, "PBase/PBController")):
+        os.mkdir(os.path.join(home, "PBase/PBController"))
+    
+    #change to the PBase/PBController directory
+    os.chdir(os.path.join(home, "PBase/PBController"))
+    
+    #check if data directory already exist, if not create it with initial 
+    if not os.path.isdir("data"):
+        shutil.copytree(os.path.join(git, "data"), "data")
+    
+    #overwrite all files except
+    for filename in os.listdir(git):
+        if os.path.isfile(os.path.join(git, filename)) and filename[0]!=".":
+            if os.path.isfile(filename):
+                os.remove(filename)
+            shutil.copy(os.path.join(git, filename), filename)
+    
+    #overwrite all folders except data
+    for foldername in os.listdir(git):
+        if os.path.isdir(os.path.join(git, foldername)) and foldername[0]!=".":
+            if foldername!="data":
+                if os.path.isdir(foldername):
+                    shutil.rmtree(foldername)
+                shutil.copytree(os.path.join(git, foldername), foldername)
+                
+install()
